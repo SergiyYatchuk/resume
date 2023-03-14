4cbd50ecd09f0d1a0f192ac3d502a64f72251918
Sergiy Yatchuk <sergeyyatchuk1992@gmail.com>
2023-03-15 01:31:44 +0200

    upd

--- src/route/index.js
+++ src/route/index.js
@@ -460,6 +460,7 @@ router.get('/bio', function (req, res) {
 router.get('/program', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('program', {
+    layout: 'basic',
     program: {
       excursion: {
         name: 'Cultural Tour',
